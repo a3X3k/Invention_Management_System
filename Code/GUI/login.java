ac0e9790e312d7ffdd4cfa100e4e9e0914d59902
S Abhishek <sabhishek@am.students.amrita.edu>
2021-07-19 22:47:52 +0530

    Update login.java

--- Code/GUI/login.java
+++ Code/GUI/login.java
@@ -119,7 +119,7 @@ public class login implements ActionListener{
 	    try 
     	{
     		Class.forName("org.postgresql.Driver");
-    		con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/invention_management_system", "postgres", "rahandbms");
+    		con = DriverManager.getConnection("<JDBC Link>", "<Username>", "<Password>");
     	}
     	catch (Exception e)
     	{
