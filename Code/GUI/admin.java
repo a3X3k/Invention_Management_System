ac0e4734fd2d4882a11cad1755476205b4648a3e
S Abhishek <sabhishek@am.students.amrita.edu>
2021-07-19 22:44:06 +0530

    Update admin.java

--- Code/GUI/admin.java
+++ Code/GUI/admin.java
@@ -279,7 +279,7 @@ public class admin implements ActionListener{
 	    try 
     	{
     		Class.forName("org.postgresql.Driver");
-    		con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/invention_management_system", "postgres", "rahandbms");
+    		con = DriverManager.getConnection("<JDBC Link>", "<Username>", "<Password>");
     	}
     	catch (Exception e)
     	{
