c1d1139ffe9ae5be24aa4e38c61ac689b0cea83c
S Abhishek <sabhishek@am.students.amrita.edu>
2021-07-19 22:46:32 +0530

    Update inventor.java

--- Code/GUI/inventor.java
+++ Code/GUI/inventor.java
@@ -156,7 +156,7 @@ public class inventor implements ActionListener {
 	        try 
 	    	{
 	    		Class.forName("org.postgresql.Driver");
-	    		con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/invention_management_system", "postgres", "rahandbms");
+	    		con = DriverManager.getConnection("<JDBC Link>", "<Username>", "<Password>");
 	    	}
 	    	catch (Exception e)
 	    	{
