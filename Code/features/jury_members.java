130febce00345b34f985a37313cf1d3bf55b4d6a
S Abhishek <sabhishek@am.students.amrita.edu>
2021-07-19 22:54:22 +0530

    Update jury_members.java

--- Code/features/jury_members.java
+++ Code/features/jury_members.java
@@ -23,7 +23,7 @@ public class jury_members extends user
 		try 
     	{
     		Class.forName("org.postgresql.Driver");
-    		con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/invention_management_system", "postgres", "rahandbms");
+    		con = DriverManager.getConnection("<JDBC Link>", "<Username>", "<Password>");
     	}
     	catch (Exception e)
     	{
